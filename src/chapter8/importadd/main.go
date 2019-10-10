d94fa07f117eb04110522e4bdb799c56a5d8a9da
anziguoer <anziguoer@163.com>
2019-10-10 10:28:48 +0800

    update

--- src/chapter8/importadd/main.go
+++ src/chapter8/importadd/main.go
@@ -1,1 +1,10 @@
-package importadd
+package main
+
+import (
+	"chapter8/importadd/mylib"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(mylib.Add(1,2))
+}
--- src/hello/main.go
+++ src/hello/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		request.ParseForm()
+		name := request.PostForm.Get("name")
+		fmt.Println(name)
+		res := `{"name":"`+name+`"}`
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(res))
+	})
+	http.ListenAndServe(":8080", nil)
 }
 
