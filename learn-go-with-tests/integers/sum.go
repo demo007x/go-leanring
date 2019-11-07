3f8b07d205c5834e6a9a6bac999b82c406c01f98
anziguoer <anziguoer@163.com>
2019-11-07 16:47:13 +0800

    test

--- learn-go-with-tests/integers/sum.go
+++ learn-go-with-tests/integers/sum.go
@@ -1,1 +1,56 @@
-package main
+package integers
+
+import "fmt"
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	// 获取传递的参数的长度
+	// lengthOfNumbers := len(numbersToSum)
+
+	// sums = make([]int, lengthOfNumbers)
+	// sums = make([]int, lengthOfNumbers)
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	var sums []int
+	for _, numbers := range numbersToSum {
+		s := Sum(numbers)
+		fmt.Println(s)
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+func main() {
+	len := 4
+	a := []int{1, 2}
+	b := [3]int{1, 2}
+	c := make([]int, len)
+
+	fmt.Printf("%T %#v,%T %#v,%T %#v", a, a, b, b, c, c)
+}
--- learn-go-with-tests/integers/sum_test.go
+++ learn-go-with-tests/integers/sum_test.go
@@ -1,12 +1,73 @@
-package main
+package integers
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+// func TestSum(t *testing.T) {
+// 	numbers := [5]int{1, 2, 3, 4, 5}
+// 	got := Sum(numbers)
+// 	want := 15
+// 	if want != got {
+// 		t.Errorf("got %d wnat %d given, %v", got, want, numbers)
+// 	}
+// }
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if want != got {
-		t.Errorf("got %d wnat %d given, %v", got, want, numbers)
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d wnat %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d wnat %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// want := "bob"
+	// 不能对两个切片使用 == 来判断是否相同
+	// 需要使用 reflect.DeepEqual 来比较
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	// got := SumAllTails([]int{1, 2}, []int{0, 9})
+	// want := []int{2, 9}
+
+	// if !reflect.DeepEqual(got, want) {
+	// 	t.Errorf("got %v, wnat %v", got, want)
+	// }
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of sole slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
 }
