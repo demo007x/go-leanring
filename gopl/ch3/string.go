d1bad8dd436c25df342b8fa2a212ed55082e3484
杨玉龙 <anziguoer@163.com>
2019-12-19 08:46:03 +0800

    update

--- gopl/ch3/string.go
+++ gopl/ch3/string.go
@@ -26,10 +26,15 @@ func main() {
 	fmt.Println(len(s))
 	fmt.Println(utf8.RuneCountInString(s))
 
-	for i := 0; i < len(s); {
-		r, size := utf8.DecodeRuneInString(s[i:])
-		fmt.Printf("%d\t%c\n", i, r)
-		i += size
+
+	//for i := 0; i < len(s); {
+	//	r, size := utf8.DecodeRuneInString(s[i:])
+	//	fmt.Printf("%d\t%c\n", i, r)
+	//	i += size
+	//}
+
+	for i, r := range s {
+		fmt.Printf("%d\t%q\t%d\n", i, r,r)
 	}
 }
 
